772c7e17f81da3527a9cc89925f32b6b67955bf3
Paulo Oliveira <111186037+p-oliveira7@users.noreply.github.com>
2023-07-10 17:58:31 -0300

    mapeando minha entidade tabela de produtos

--- src/main/java/br/com/alura/loja/modelo/Produto.java
+++ src/main/java/br/com/alura/loja/modelo/Produto.java
@@ -1,7 +1,16 @@
 package br.com.alura.loja.modelo;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.math.BigDecimal;
 
 @Entity
+@Table(name = "produtos")
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+
 }
